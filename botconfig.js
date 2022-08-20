3a1f0ed1bb00e2b9f17d6ce53c3fdb3b496e20b6
Saito <markungphim@gmail.com>
2022-08-21 01:39:06 +0200

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -1,17 +1,17 @@
 module.exports = {
-  Admins: ["848917797501141052"], // Admins of the bot, I don't know what this do. -Darren.
+  Admins: ["UserID", "UserID"], // Admins of the bot, I don't know what this do. -Darren.
   ExpressServer: true, // If you wanted to make the website run or not
-  DefaultPrefix: process.env.Prefix || "a!", // Default prefix, Server Admins can change the prefix
+  DefaultPrefix: process.env.Prefix || ">", // Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
-  SupportServer: "https://discord.io/Inferno-World", // Support Server Link
-  Token: process.env.Token || "OTA5ODAyMDg2NjA1MTMxODQ3.YZJlOA.iYk0Ipc6ocIV4VGl-GvOG6Q341M", // Discord Bot Token
-  ClientID: process.env.Discord_ClientID || "909802086605131847", // Discord Client ID
-  ClientSecret: process.env.Discord_ClientSecret || "fhGY4q6YAwOhM70_EzOgX3CdHlg_z6R6", // Discord Client Secret
+  SupportServer: "https://discord.gg/sbySMS7m3v", // Support Server Link
+  Token: process.env.Token || "", // Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "", // Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "", // Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], // Discord OAuth2 Scopes
   ServerDeafen: true, // If you want bot to stay deafened
   DefaultVolume: 100, // Sets the default volume of the bot, You can change this number anywhere from 1 to 9007199254740991 (JS Integer limit. If you do set it to that, you're a monster.)
   CallbackURL: "/api/callback", // Discord API Callback url. Do not touch it if you don't know what you are doing. All you need to change for website to work is on line 20.
-  "24/7": true, // Make the bot stays in VC 24/7 (when you reboot the bot will **not** automatically rejoin.)
+  "24/7": false, // Make the bot stays in VC 24/7 (when you reboot the bot will **not** automatically rejoin.)
   CookieSecret: "Pikachu is cute", // A cookie for you, cookie for me. make sure you change this value!
   IconURL:
     "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", // URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
@@ -30,16 +30,16 @@ module.exports = {
   // Lavalink server; public lavalink -> https://lavalink-list.darrennathanael.com/; create one yourself -> https://darrennathanael.com/post/how-to-lavalink
   Lavalink: {
     id: "Main", //- Used for indentifier. You can set this to whatever you want.
-    host: "node1.kartadharta.xyz", //- The host name or IP of the lavalink server.
-    port: 443, // The port that lavalink is listening to. This must be a number!
-    pass: "kdlavalink", //- The password of the lavalink server.
-    secure: true, // Set this to true if the lavalink uses SSL. if not set it to false.
+    host: "", //- The host name or IP of the lavalink server.
+    port: 80, // The port that lavalink is listening to. This must be a number!
+    pass: "", //- The password of the lavalink server.
+    secure: false, // Set this to true if the lavalink uses SSL. if not set it to false.
     retryAmount: 200, //- The amount of times to retry connecting to the node if connection got dropped.
     retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
   },
   // Spotify Integration, allows you to enter a spotify link.
   Spotify: {
-    ClientID: process.env.Spotify_ClientID || "a4e2e9cca2254f06a5154b86365b2eb4", // Spotify Client ID
-    ClientSecret: process.env.Spotify_ClientSecret || "0bd6c2d5b9ca4808bdc377e07692e613", // Spotify Client Secret
+    ClientID: process.env.Spotify_ClientID || "", // Spotify Client ID
+    ClientSecret: process.env.Spotify_ClientSecret || "", // Spotify Client Secret
   },
 };
